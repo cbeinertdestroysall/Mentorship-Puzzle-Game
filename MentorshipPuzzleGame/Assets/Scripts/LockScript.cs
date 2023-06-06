9b83a041c6e58a290e54f1529ada72a80e4d9706
Cody Beinert <codybeinert123@gmail.com>
2023-06-06 12:22:01 -0400

    attempt to change lock value using arrays (fail)

---
+++ MentorshipPuzzleGame/Assets/Scripts/LockScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScript : MonoBehaviour
+{
+    public GameObject[] lockNumbers;
+    public int currentLockNumber = 1;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //attempting to make the object in the array that shares the number in array as currentLockNumber set active
+        /*for (int i = 0; i < 8; i++)
+        {
+            if (lockNumbers[i].GetComponent<float>() == currentLockNumber)
+            {
+                lockNumbers[i].SetActive(true);
+            }
+            else 
+            {
+                lockNumbers[i].SetActive(false);
+            }
+        }*/
+
+
+        lockNumbers[currentLockNumber].SetActive(true);
+        lockNumbers[currentLockNumber].SetActive(false);
+    }
+
+    public void IncreaseLockNumber()
+    {
+        if (currentLockNumber < 9)
+        {
+            currentLockNumber+= 1;
+        }
+        else if (currentLockNumber >= 9)
+        {
+            currentLockNumber = 0;
+        }
+    }
+
+    public void DecreaseLockNumber()
+    {
+        if (currentLockNumber > 0)
+        {
+            currentLockNumber-= 1;
+        }
+        else if (currentLockNumber <= 0)
+        {
+            currentLockNumber = 9;
+        }
+    }
+}
