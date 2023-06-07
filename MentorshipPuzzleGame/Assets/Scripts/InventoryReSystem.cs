b8f3c41464cc2f80a03262e2cf95eccc92a96b6a
Cody Beinert <codybeinert123@gmail.com>
2023-06-07 10:09:37 -0400

    clean up inventory script

--- MentorshipPuzzleGame/Assets/Scripts/InventoryReSystem.cs
+++ MentorshipPuzzleGame/Assets/Scripts/InventoryReSystem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class InventoryReSystem : MonoBehaviour
 {
-    public int slotNumber = 1;
+    public int slotNumber = 0;
     
 
     // Start is called before the first frame update
--- MentorshipPuzzleGame/Assets/Scripts/ItemReScript.cs
+++ MentorshipPuzzleGame/Assets/Scripts/ItemReScript.cs
@@ -10,6 +10,8 @@ public class ItemReScript : MonoBehaviour
     public GameObject slot2;
     public GameObject slot3;
 
+    public GameObject[] slots;
+
 
 
     // Start is called before the first frame update
@@ -26,24 +28,18 @@ public class ItemReScript : MonoBehaviour
 
     public void AddItemToSlot()
     {
-        if (mainFunctionality.GetComponent<InventoryReSystem>().slotNumber == 1)
+       
+        for (int i = 0; i < slots.Length; i++)
         {
-            Debug.Log("item added 1");
-            item.transform.position = slot1.transform.position;
-            item.transform.SetParent(slot1.transform.parent, true);
+            if (i == mainFunctionality.GetComponent<InventoryReSystem>().slotNumber)
+            {
+                Debug.Log("item added " + mainFunctionality.GetComponent<InventoryReSystem>().slotNumber);
+                item.transform.position = slots[i].transform.position;
+                item.transform.SetParent(slots[i].transform.parent, true);
+               
+            }
         }
-        else if (mainFunctionality.GetComponent<InventoryReSystem>().slotNumber == 2)
-        {
-            Debug.Log("item added 2");
-            item.transform.position = slot2.transform.position;
-            item.transform.SetParent(slot2.transform.parent, true);
-        }
-        else if (mainFunctionality.GetComponent<InventoryReSystem>().slotNumber == 3)
-        {
-            Debug.Log("item added 3");
-            item.transform.position = slot3.transform.position;
-            item.transform.SetParent(slot3.transform.parent, true);
-        }
+     
     }
 
    
