aa83b94cd0ad9c6d90477f94ed06ebbea2c523fe
Cody Beinert <codybeinert123@gmail.com>
2023-06-14 10:00:23 -0400

    get inventory system almost working

--- MentorshipPuzzleGame/Assets/Scripts/ItemReScript.cs
+++ MentorshipPuzzleGame/Assets/Scripts/ItemReScript.cs
@@ -29,7 +29,7 @@ public class ItemReScript : MonoBehaviour
        
         for (int i = 0; i < slots.Length; i++)
         {
-            if (i == mainFunctionality.GetComponent<InventoryReSystem>().slotNumber)
+            if (i == mainFunctionality.GetComponent<InventoryReSystem>().slotNumber && slots[i].GetComponent<FillSlot>().slotIsFilled == false)
             {
                 Debug.Log("item added " + mainFunctionality.GetComponent<InventoryReSystem>().slotNumber);
                 item.transform.position = slots[i].transform.position;
@@ -37,7 +37,22 @@ public class ItemReScript : MonoBehaviour
                 item.transform.SetParent(slots[i].transform.parent, true);
                 item.GetComponent<ItemScript>().draggable = true;
             }
+            else if (i == mainFunctionality.GetComponent<InventoryReSystem>().slotNumber && slots[i].GetComponent<FillSlot>().slotIsFilled == true)
+            {
+                if (i < slots.Length)
+                {
+                    mainFunctionality.GetComponent<InventoryReSystem>().IncreaseSlotNumber();
+                }
+                else if (i >= slots.Length)
+                {
+                    mainFunctionality.GetComponent<InventoryReSystem>().DecreaseSlotNumber();
+                }
             
+                  
+
+            }
+
+
         }
      
     }
--- MentorshipPuzzleGame/Assets/Scripts/PuzzlePieceScript.cs
+++ MentorshipPuzzleGame/Assets/Scripts/PuzzlePieceScript.cs
@@ -34,9 +34,12 @@ public class PuzzlePieceScript : MonoBehaviour
         }
         else if (collision.gameObject.tag == "Inventory")
         {
-            this.GetComponent<InventoryPosition>().inventoryPos = collision.gameObject.GetComponent<BoxCollider2D>().transform.position;
-
-            this.transform.SetParent(collision.transform.parent, true);
+            if (collision.GetComponent<FillSlot>().slotIsFilled == false)
+            {
+                this.GetComponent<InventoryPosition>().inventoryPos = collision.gameObject.GetComponent<BoxCollider2D>().transform.position;
+                collision.GetComponent<FillSlot>().slotIsFilled = true;
+                this.transform.SetParent(collision.transform.parent, true);
+            }
         }
     }
 
@@ -53,6 +56,7 @@ public class PuzzlePieceScript : MonoBehaviour
         if (collision.gameObject.tag == "Inventory")
         {
             mainFunctionality.GetComponent<InventoryReSystem>().DecreaseSlotNumber();
+            collision.GetComponent<FillSlot>().slotIsFilled = false;
         }
     }
 
