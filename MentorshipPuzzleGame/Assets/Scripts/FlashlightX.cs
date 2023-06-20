81148d381eb5ff7865af66de29d6e2b4d187a6d9
Cody Beinert <codybeinert123@gmail.com>
2023-06-20 12:32:29 -0400

    holy moly I managed to figure out how to make 4 objects follow the mouse with the correct offsets

--- MentorshipPuzzleGame/Assets/Scripts/FlashlightX.cs
+++ MentorshipPuzzleGame/Assets/Scripts/FlashlightX.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
-public class Flashlight : MonoBehaviour
+public class FlashlightX : MonoBehaviour
 {
     Vector2 mousePos;
-    Vector2 currentPos;
+    //Vector2 currentPos;
+
+    public Vector2 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentPos = this.transform.position;
+        //currentPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
         //currentPos = Camera.main.ScreenToWorldPoint(this.transform.position);
 
-        this.transform.position = Input.mousePosition;
+        this.transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y) + offset;
 
         //currentPos.y = mousePos.y;
 
         //this.transform.position = Input.mousePosition.y;
 
         Debug.Log("mouse Pos = " + mousePos);
-        Debug.Log("mask position = " + currentPos.y);
+        //Debug.Log("mask position = " + currentPos.y);
     }
 }
---
+++ MentorshipPuzzleGame/Assets/Scripts/FlashlightY.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class FlashlightY : MonoBehaviour
+{
+    Vector2 mousePos;
+    Vector2 currentPos;
+
+    public Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
+        //currentPos = Camera.main.ScreenToWorldPoint(this.transform.position);
+
+        this.transform.position = new Vector2(currentPos.x, Input.mousePosition.y) + offset;
+
+        //currentPos.y = mousePos.y;
+
+        //this.transform.position = Input.mousePosition.y;
+
+        Debug.Log("mouse Pos = " + mousePos);
+        Debug.Log("mask position = " + currentPos.y);
+    }
+}
