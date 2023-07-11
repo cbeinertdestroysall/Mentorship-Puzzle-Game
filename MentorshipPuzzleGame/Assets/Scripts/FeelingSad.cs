3035c05979ae222bb9337883d504941b620b149f
Cody Beinert <codybeinert123@gmail.com>
2023-07-11 12:32:18 -0400

    complete implementation of text hovering UI for key object

--- MentorshipPuzzleGame/Assets/Scripts/FeelingSad.cs
+++ MentorshipPuzzleGame/Assets/Scripts/FeelingSad.cs
@@ -7,18 +7,43 @@ using UnityEngine.InputSystem;
 
 public class FeelingSad : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    
+    public GameObject signifierText;
+
+    public bool isHovering = false;
+
+    public Vector3 offset;
 
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         //Output to console the GameObject's name and the following message
         Debug.Log("Cursor Entering " + name + " GameObject");
+        signifierText.SetActive(true);
+        isHovering = true;
+        //signifierText.transform.position = Input.mousePosition;
     }
 
-    //Detect when Cursor leaves the GameObject
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         //Output the following message with the GameObject's name
         Debug.Log("Cursor Exiting " + name + " GameObject");
+        signifierText.SetActive(false);
+        isHovering = false;
+    }
+
+    private void Update()
+    {
+        if (this == null)
+        {
+            Destroy(signifierText);
+        }
+
+        if (isHovering)
+        {
+            signifierText.transform.position = Input.mousePosition + offset;
+        }
+        else
+        {
+            signifierText.transform.position = this.transform.position;
+        }
     }
 }
