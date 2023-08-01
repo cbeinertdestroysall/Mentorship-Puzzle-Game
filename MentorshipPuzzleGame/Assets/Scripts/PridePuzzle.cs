1dbbcd343ac502e27a2e41a87d07ff00a765e514
Cody Beinert <codybeinert123@gmail.com>
2023-08-01 15:18:55 -0400

    implement sound that plays when you solve a puzzle

--- MentorshipPuzzleGame/Assets/Scripts/PridePuzzle.cs
+++ MentorshipPuzzleGame/Assets/Scripts/PridePuzzle.cs
@@ -10,6 +10,8 @@ public class PridePuzzle : MonoBehaviour
 
     public GameObject door;
 
+    public AudioSource youDidIt;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,6 +49,11 @@ public class PridePuzzle : MonoBehaviour
     public void ActivateDoor()
     {
         if (puzzleSolved)
-        door.SetActive(true);
+        {
+            door.SetActive(true);
+            youDidIt.Play();
+        }
     }
+
+
 }
